20ad7d1822eab9f97f3ecab4233970d4eb123a6b
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-04-11 16:12:49 +0100

    bug: apollo server not starting

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,5 +1,6 @@
 import { UserModel, BankAccountModel } from "../models/index.js";
 import { paystackTestSecret } from "../../config/config.js";
+import { levenshteinDistance } from "../utils/index.js";
 
 const PAYSTACK_AUTHORIZATION_HEADER = `Bearer ${paystackTestSecret}`;
 const MAXIMUM_ALLOW_LEVENSHTEIN_DISTANCE = 2;
@@ -37,7 +38,67 @@ export const resolvers = {
   },
   Mutation: {
     async addUser(_, { email, password, phone }) {
-        
+      const user = new UserModel({
+        email,
+        phone,
+        password,
+      });
+      return user.save();
+    },
+    async deleteUser(_, { id }) {
+      return await UserModel.findByIdAndDelete(id);
+    },
+    async addAccount(_, args) {
+      try {
+        const user = await UserModel.findById(args?.user_id);
+        // Check if the user Id provided belongs to a user
+        if (!user) {
+          throw new Error("No user found with the id provided");
+        }
+
+        const response = await axios.get(
+          `https://api.paystack.co/bank/resolve?account_number=${args?.user_account_number}&bank_code=${args?.user_bank_code}`,
+          {
+            headers: {
+              Authorization: PAYSTACK_AUTHORIZATION_HEADER,
+            },
+          }
+        );
+
+        if (response?.status !== 200) {
+          throw new Error(`Something went wrong`);
+        }
+
+        const isLevenshteinDistanceWithinAllowableLimit =
+          MAXIMUM_ALLOW_LEVENSHTEIN_DISTANCE >=
+          levenshteinDistance(
+            args?.user_account_name?.toUpperCase(),
+            response?.data?.data?.account_name?.toUpperCase()
+          );
+
+        if (!isLevenshteinDistanceWithinAllowableLimit) {
+          throw new Error(
+            "Account name provided doesn't match the account name linked to this account number"
+          );
+        }
+
+        const newAccount = new BankAccountModel({
+          user_account_name: response?.data?.data?.account_name,
+          user_id: args?.user_id,
+          user_account_number: args?.user_account_number,
+          user_bank_code: args?.user_bank_code,
+          user_bank_id: response?.data?.data?.bank_id,
+        });
+
+        return newAccount.save();
+      } catch (error) {
+        if (error?.response) {
+          throw new Error(
+            `Something went wrong ${error?.response?.data?.message}`
+          );
+        }
+        throw new Error(`Something went wrong: ${error?.message}`);
+      }
     },
   },
 };
